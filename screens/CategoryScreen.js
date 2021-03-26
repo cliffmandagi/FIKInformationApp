dfa0573f3119516d7118bd90c33e8ff4da94abd2
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2021-03-26 22:59:46 +0800

    Add New Screen
    Add Bookmark Screen
    Add Category Screen

---
+++ screens/CategoryScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const CategoryScreen = () => {
+  return (
+    <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
+      <Text>Category Screen</Text>
+    </View>
+  );
+};
+
+export default CategoryScreen;
